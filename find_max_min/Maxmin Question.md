c1789ee03e5a5f6210576eabc64cf3ebb336d211
Sharonsyra <sharonwaithii@gmail.com>
2018-04-16 16:05:19 +0300

    Find minimum and maximum number

---
+++ find_max_min/Maxmin Question.md
@@ -0,0 +1,3 @@
+# find_max_min
+
+Your answer should be returned in an array containing the min and max number, respectively.
---
+++ find_max_min/maxmin.py
@@ -0,0 +1,10 @@
+def find_max_min(rob):
+  #function to create method find_max_min with an argument rob
+    rob_list=[]
+    if min(rob)!=max(rob):
+        rob_list.append(min(rob))
+        rob_list.append(max(rob))
+        return rob_list
+    else:
+        rob_list.append(len(rob))
+        return rob_list
