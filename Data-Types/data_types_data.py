f3df951456d70d094041b1698a28bbc941a2b1c4
Sharonsyra <sharonwaithii@gmail.com>
2018-04-16 16:03:15 +0300

    Data Types

---
+++ Data-Types/data_types_data.py
@@ -0,0 +1,18 @@
+def data_type(num):
+	if type(num) is str:
+		return len(num)
+	elif num is None:
+		return "no value"
+	elif type(num) is bool:
+		return num
+	elif type(num) is int:
+		if num < 100:
+			return "less than 100"
+		elif num ==100:
+			return "equal to 100" 
+		return "more than 100"
+	elif type(num) is list:
+		if len(num) < 3:
+			return None
+		else:
+			return num[2]
